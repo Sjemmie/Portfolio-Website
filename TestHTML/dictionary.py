5af7b50bb375e9a66293f85b9dc6b0808bfaf0d1
Youri <youridibbet@gmail.com>
2023-03-06 11:43:35 +0100

    Added dynamic word functionality for dictionary API

--- TestHTML/dictionary.py
+++ TestHTML/dictionary.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
+
+df = pd.read_csv(r"C:\Users\youri\PycharmProjects\Portfolio Website\TestHTML\dictionary.csv")
 
 
 @app.route("/")
@@ -10,7 +13,8 @@ def home():
 
 @app.route("/dict/v1/<word>")
 def definition(word):
-    return {"definition": word.upper,
+    def_01 = df.loc[df["word"] == word]["definition"].squeeze()
+    return {"definition": str(def_01),
             "word": word
             }
 

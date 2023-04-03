ab6dbc21f42bd9963db232d75bbcae01d438a2ee
Youri <youridibbet@gmail.com>
2023-04-03 13:31:33 +0200

    First test with PyQT6 making a age calculator

---
+++ StudentManagementSystem/example.py
@@ -0,0 +1,44 @@
+import sys
+from datetime import datetime
+from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, QLineEdit, QPushButton
+
+
+class AgeCalculator(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Age Calculator")
+        grid = QGridLayout()
+
+        # Create widgets
+        name_label = QLabel("Name: ")
+        self.name_label_edit = QLineEdit()
+
+        date_birth_label = QLabel("Date of Birth DD/MM/YYYY: ")
+        self.date_birth_edit = QLineEdit()
+
+        calculate_button = QPushButton("Calculate Age")
+        calculate_button.clicked.connect(self.calculate_age)
+        self.output_label = QLabel("")
+
+        # Add widgets to grid
+        grid.addWidget(name_label, 0, 0)
+        grid.addWidget(self.name_label_edit, 0, 1)
+        grid.addWidget(date_birth_label, 1, 0)
+        grid.addWidget(self.date_birth_edit, 1, 1)
+        grid.addWidget(calculate_button, 2, 0, 1, 2)
+        grid.addWidget(self.output_label, 3, 0, 1, 2)
+
+        self.setLayout(grid)
+
+    def calculate_age(self):
+        current_year = datetime.now().year
+        date_of_birth = self.date_birth_edit.text()
+        year_of_birth = datetime.strptime(date_of_birth, "%d/%m/%Y").date().year
+        age = current_year - year_of_birth
+        self.output_label.setText(f"{self.name_label_edit.text()} is {age} years old.")
+
+
+app = QApplication(sys.argv)
+age_calculator = AgeCalculator()
+age_calculator.show()
+sys.exit(app.exec())
